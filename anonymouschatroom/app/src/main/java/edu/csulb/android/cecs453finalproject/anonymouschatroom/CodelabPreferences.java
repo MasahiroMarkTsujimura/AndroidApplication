e9da80c583d8393da968966bef5f5aa12955aa9d
Il Won Son <highperion1212@gmail.com>
2017-11-17 20:12:08 -0800

    AnonymousChatRoom
    Sign-in w/ google acct.
    Firebase connected to chat.

---
+++ anonymouschatroom/app/src/main/java/edu/csulb/android/cecs453finalproject/anonymouschatroom/CodelabPreferences.java
@@ -0,0 +1,7 @@
+package edu.csulb.android.cecs453finalproject.anonymouschatroom;
+
+public class CodelabPreferences {
+
+    public static final String FRIENDLY_MSG_LENGTH = "friendly_msg_length";
+
+}
---
+++ anonymouschatroom/app/src/main/java/edu/csulb/android/cecs453finalproject/anonymouschatroom/MyFirebaseInstanceIdService.java
@@ -0,0 +1,16 @@
+package edu.csulb.android.cecs453finalproject.anonymouschatroom;
+
+/**
+ * Created by ilwonson on 11/17/17.
+ */
+
+public class MyFirebaseInstanceIdService {
+    private static final String TAG = "MyFirebaseIIDService";
+    private static final String FRIENDLY_ENGAGE_TOPIC = "friendly_engage";
+
+    /**
+     * The Application's current Instance ID token is no longer valid and thus a new one must be requested.
+     */
+    public void onTokenRefresh() {
+    }
+}
