dafb7aa17c2ac4922875bbac64ca673903ff3ce0
Unknown <whathappenedman@gmail.com>
2017-12-08 17:51:20 -0800

    Cleaned repo, updated my part of readme

--- anonymouschatroom/app/src/main/java/edu/csulb/android/cecs453finalproject/anonymouschatroom/CodelabPreferences.java
+++
@@ -1,7 +0,0 @@
-package edu.csulb.android.cecs453finalproject.anonymouschatroom;
-
-public class CodelabPreferences {
-
-    public static final String FRIENDLY_MSG_LENGTH = "friendly_msg_length";
-
-}
